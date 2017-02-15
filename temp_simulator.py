f9ebefd705e973da38608d3819fc497dd2e227e0
mudits1990 <mudit.s@shawacademy.com>
2017-02-15 10:28:18 +0530

    Updated temp simulator code.

--- temp_simulator.py
+++ temp_simulator.py
@@ -1,7 +1,10 @@
 import requests
 import random
 import grequests
+import time
 
 if __name__ == '__main__':
-	rs = (grequests.post('http://127.0.0.1:8888/addtemp', data={'message': random.randint(1,100)}) for u in xrange(0,1000))
-	grequests.map(rs)
+	while True:
+		rs = (grequests.post('http://127.0.0.1:8888/addtemp', data={'message': random.randint(1,100)}) for u in xrange(0,1000))
+		grequests.map(rs)
+		time.sleep(5)
